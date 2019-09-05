e909bd9b192a9cba6c63364b9331f6812d3b36d8
christian williams <christian.williamsj@gmail.com>
2019-09-05 05:22:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
     remove_element_from_end_of_array = [1, 2, 3, 4]
-    remove_element_from_end_of_array
+    remove_element_from_end_of_array.pop ("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
